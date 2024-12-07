c3b96afdc4810c1d68dbc5ae76c9660bfa3389c3
Luca Neukom <luca.neukom@gmail.com>
2024-12-07 10:49:40 +0100

    create COLON_SPLITTER

--- java/src/main/java/ch/neukom/advent2024/util/splitter/Splitters.java
+++ java/src/main/java/ch/neukom/advent2024/util/splitter/Splitters.java
@@ -5,6 +5,7 @@ import com.google.common.base.Splitter;
 public class Splitters {
     public static final Splitter COMMA_SPLITTER = Splitter.on(',').omitEmptyStrings().trimResults();
     public static final Splitter WHITESPACE_SPLITTER = Splitter.on(' ').trimResults().omitEmptyStrings();
+    public static final Splitter COLON_SPLITTER = Splitter.on(':').trimResults().omitEmptyStrings();
 
     private Splitters() {
     }
