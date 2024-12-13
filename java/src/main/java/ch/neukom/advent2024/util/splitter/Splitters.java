f74adbf1f22f85e82ccc8ac374fa9d82012043b6
Luca Neukom <luca.neukom@gmail.com>
2024-12-13 17:35:42 +0100

    add paragraph and newline splitters

--- java/src/main/java/ch/neukom/advent2024/util/splitter/Splitters.java
+++ java/src/main/java/ch/neukom/advent2024/util/splitter/Splitters.java
@@ -6,6 +6,8 @@ public class Splitters {
     public static final Splitter COMMA_SPLITTER = Splitter.on(',').omitEmptyStrings().trimResults();
     public static final Splitter WHITESPACE_SPLITTER = Splitter.on(' ').trimResults().omitEmptyStrings();
     public static final Splitter COLON_SPLITTER = Splitter.on(':').trimResults().omitEmptyStrings();
+    public static final Splitter PARAGRAPH_SPLITTER = Splitter.on("\n\n").trimResults().omitEmptyStrings();
+    public static final Splitter NEWLINE_SPLITTER = Splitter.on("\n").trimResults().omitEmptyStrings();
 
     private Splitters() {
     }
